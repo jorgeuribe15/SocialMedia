97a16a7e834c3f4ebf5afbe0516d4226141e0d77
Jorge Uribe <jorgeuribe21@gmail.com>
2022-08-05 07:53:09 -0700

    Add Commands

---
+++ SM-Post/Post.Cmd/Post.Cmd.Api/Commands/AddCommnetCommand.cs
@@ -0,0 +1,10 @@
+﻿using CQRS.Core.Commands;
+
+namespace Post.Cmd.Api.Commands
+{
+    public class AddCommnetCommand : BaseCommand
+    {
+        public string Commnet { get; set; }
+        public string Username { get; set; }
+    }
+}
---
+++ SM-Post/Post.Cmd/Post.Cmd.Api/Commands/EditCommnetCommand.cs
@@ -0,0 +1,11 @@
+﻿using CQRS.Core.Commands;
+
+namespace Post.Cmd.Api.Commands
+{
+    public class EditCommnetCommand : BaseCommand
+    {
+        public Guid CommnetId { get; set; }
+        public string Commnent { get; set; }
+        public string Username { get; set; }
+    }
+}
