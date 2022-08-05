7bb714fbbd209c9815bb990190775885c82bdf70
Jorge Uribe <jorgeuribe21@gmail.com>
2022-08-05 08:26:56 -0700

    Add Events structure

---
+++ SM-Post/Post.Common/Events/CommentAddEvent.cs
@@ -0,0 +1,22 @@
+﻿using CQRS.Core.Events;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Post.Common.Events
+{
+    public class CommentAddEvent : BaseEvent
+    {
+        public CommentAddEvent() : base(nameof(CommentAddEvent))
+        {
+        }
+
+        public Guid CommentId { get; set; }
+        public string Comment { get; set; }
+        public string Username { get; set; }
+        public DateTime CommentDate { get; set; }
+
+    }
+}
---
+++ SM-Post/Post.Common/Events/CommentUpdateEvent.cs
@@ -0,0 +1,21 @@
+﻿using CQRS.Core.Events;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Post.Common.Events
+{
+    public class CommentUpdateEvent : BaseEvent
+    {
+        public CommentUpdateEvent() : base(nameof(CommentUpdateEvent))
+        {
+        }
+
+        public Guid CommentId { get; set; }
+        public string Comment { get; set; }
+        public string Username { get; set; }
+        public DateTime EditDate { get; set; }
+    }
+}
