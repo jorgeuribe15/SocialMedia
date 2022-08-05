a00c8fbbe642f3599b21d34b891849ccd8fe0b08
Jorge Uribe <jorgeuribe21@gmail.com>
2022-08-05 09:39:02 -0700

    Add Dispatchers infrastructure

---
+++ CQRS-ES/CQRS.Core/Infrastructure/ICommandDispatcher.cs
@@ -0,0 +1,28 @@
+﻿using CQRS.Core.Commands;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CQRS.Core.Infrastructure
+{
+
+    /// <summary>
+    /// Concrete Mediator
+    /// </summary>
+    public interface ICommandDispatcher
+    {
+        /// <summary>
+        /// T is an in parameter
+        /// Task is an out parameter        
+        void RegisterHandler<T>(Func<T, Task> handler) where T : BaseCommand;
+
+        /// <summary>
+        /// Using Liskow substitution - Concrete class should be sustituible by Base class
+        /// </summary>
+        Task SendAsync(BaseCommand command);
+
+
+    }
+}
